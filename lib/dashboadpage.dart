d7a1b2faedd856ce978d0c561954d24d784db2ca
Pablofr10 <rafanet.live@gmail.com>
2021-03-17 01:56:05 -0300

    Adicionando App Bar

--- lib/dashboadpage.dart
+++ lib/dashboadpage.dart
@@ -8,8 +8,39 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu, color: Colors.black),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 6.0,
+                        spreadRadius: 4.0,
+                        offset: Offset(0.0, 3.0),
+                      )
+                    ],
+                    color: Color(0xFFC6E7FE),
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage('assets/tuxedo.png'),
+                        fit: BoxFit.contain),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
