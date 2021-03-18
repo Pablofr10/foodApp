cf6fd67a042664080b3c5087c611a75fabc4e59f
Pablofr10 <rafanet.live@gmail.com>
2021-03-17 21:52:00 -0300

    adicionando lista horizontal.

--- lib/dashboadpage.dart
+++ lib/dashboadpage.dart
@@ -89,8 +89,86 @@ class _DashboardPageState extends State<DashboardPage> {
               ),
             ),
           ),
-          SizedBox(height: 15.0)
+          SizedBox(height: 15.0),
+          Container(
+            height: 200.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _buildListItem(
+                  'Hamburg',
+                  'assets/burger.png',
+                  '21',
+                  Color(0xFFF5DEB3),
+                  Color(0xFFA0522D),
+                ),
+                _buildListItem(
+                  'Chips',
+                  'assets/fries.png',
+                  '15',
+                  Color(0xFFB0E0E6),
+                  Color(0xFFA0522D),
+                ),
+                _buildListItem(
+                  'Donuts',
+                  'assets/doughnut.png',
+                  '21',
+                  Color(0xFFADFF2F),
+                  Color(0xFFA0522D),
+                )
+              ],
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  _buildListItem(String foodName, String imgPath, String price, Color bgColor,
+      Color textColor) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15.0),
+      child: InkWell(
+        onTap: () {
+          // Todo
+        },
+        child: Container(
+          height: 175.0,
+          width: 150.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: bgColor,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: foodName,
+                child: Container(
+                  height: 75.0,
+                  width: 75.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: Center(
+                    child: Image.asset(imgPath, height: 50.0, width: 50.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Text(
+                foodName,
+                style: GoogleFonts.notoSans(fontSize: 17.0, color: textColor),
+              ),
+              Text(
+                '\$' + price,
+                style: GoogleFonts.notoSans(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
